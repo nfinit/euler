80fd534c3714e56ec305df7ddf07038a32139c4e
ict <ict@nfinit.systems>
2019-10-22 15:31:21 -0600

    P1: Optimization progress (C89)
    Further progress on implementing the arithmetic sequence solution for an arbitrary number of divisors. Sequence solution will count some values multiple times when they are divisible by more than one given divisor, requiring a table of possible products to be generated in order to adequately correct the end sum. Current product table function should generate all possible products of the first value of the divisor set multiplied by any other value or subset of values, but will not do the same for subsequent values in the set. A new loop is likely required to make this possible.

--- c89/p1/multisum.c
+++ c89/p1/multisum.c
@@ -16,22 +16,71 @@ typedef unsigned long DATA;
 typedef long double DATA;
 #endif
 
-#ifndef _P1_FAST
-char is_multiple (long double n, int valc, long double *values);
+
+
+#ifdef _P1_FAST
+DATA factorial (DATA n);
+DATA combination (DATA n, DATA r);
+DATA *product_array (DATA *values, unsigned long num_values);
+#else
+char is_multiple (DATA n, DATA *divisors, unsigned long num_divisors);
 #endif
 
 /* BEGIN PROGRAM LOGIC */
 
-/* is_multiple(n,values,num_values)
+#ifdef _P1_FAST
+/* factorial(n)
+ * Returns the factorial of n. One-line ternary implementation from
+ * https://www.geeksforgeeks.org/program-for-factorial-of-a-number/
+ * pending later optimization.
+ */
+DATA factorial (DATA n)
+{ return (n==1 || n==0) ? 1: n * factorial(n - 1); }
+
+/* combination(n)
+ * Returns the number of possible combinations of r objects
+ * from a set of n objects.
+ */
+DATA combination (DATA n, DATA r)
+{ return factorial(n)/(factorial(r)*factorial(n-r)); }
+  
+/* product_array(values,num_values)
+ * Creates an array containing all possible products of every value
+ * within the given input. Used to eliminate over-matched numbers
+ * when using the fast algorithm.
+ */
+DATA *product_array (DATA *values, unsigned long num_values)
+{
+  DATA *products, *temp;
+  unsigned long num_products, current_product, n, i, level;
+  current_product = 0;
+  for (n = 1; n <= num_values; n++) num_products += combination(num_values,n);
+  num_products -= num_values;
+  products = malloc(num_products*sizeof(DATA));
+  temp = malloc((num_values)*sizeof(DATA));
+  for (i = 0; i < num_values; i++) temp[i] = values[i];
+  /* TODO: The loop below only finds multiples of the first
+   * divisor. It probably needs to be enclosed in another to
+   * cover all other divisors.
+   */
+  for (level = 0; level < num_values; level++)
+  {
+    for (i = 0; i < num_values; i++) temp[i] *= values[level];
+    for (i = level+1; level+1 < num_values; i++) 
+    { products[current_product] = temp[i]; current_product++; }
+  }
+  free(temp);
+  return products;
+}
+#else
+/* is_multiple(n,divisors,num_divisors)
  * Checks if a given number is a multiple of any one (or more) of the
  * arbitrary values given in an array.
  */
-#ifndef _P1_FAST
-char is_multiple (long double n, int valc, long double *values)
-{
-  int v;
-  for (v = 0; v < valc; v++) if (fmod(n,values[v]) == 0) return 1;
-  return 0;
+char is_multiple (DATA n, DATA *divisors, unsigned long num_divisors)
+{ 
+  int i; for (i = 0; i < num_divisors; i++) if (fmod(n,divisors[i]) == 0) return 1; 
+  return 0; 
 }
 #endif
 
@@ -46,6 +95,9 @@ int main (int argc, char **argv)
   DATA max, sum, n, *divisors;
   #ifndef _P1_FAST
   DATA matches;
+  #else
+  DATA duplicate_sum, product;
+  unsigned long j;
   #endif
   unsigned long i, num_divisors, last_sum, overflows;
   sum = 0; overflows = 0;
@@ -77,10 +129,14 @@ int main (int argc, char **argv)
   #endif
   printf("Data size:                %lu bits\n",(unsigned long)sizeof(DATA)*8);
 
-  /* Main computation loop */
+  /* Main computation loops */
   #ifdef _P1_FAST
   for (i = 0; i < num_divisors; i++)
   { last_sum = sum; n = divisors[i]; sum += n*(max/n)*(((max/n)+1)/2); if (sum < last_sum) overflows++; }
+  for (j = num_divisors; j > num_divisors; j++)
+  {
+    
+  }
   #else
   for (n = 1; n < max; n++) if (is_multiple(n,argc-2,divisors)) 
   { last_sum = sum; matches++; sum += n; if (sum < last_sum) overflows++; }
