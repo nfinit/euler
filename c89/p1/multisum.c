abdd4f0d97996c77bb4c8bbf5890f5952162e166
ict <ict@nfinit.systems>
2019-10-23 22:10:25 -0600

    P1: Refactoring/makefile update (C89)
    Refactored brute-force P1 code and updated makefile to reflect temporary removal of optimized routines

--- c89/p1/multisum.c
+++ c89/p1/multisum.c
@@ -4,26 +4,28 @@
  * Find the sum of all the multiples of 3 or 5 below 1000.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/* INCLUDES, DEFINITIONS AND FUNCTION PROTOTYPES */
-
+/* INCLUDES */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+/* TYPE DEFINITIONS */
 typedef long double DATA;
+#define DATA_FORMAT "%.0Lf"
 
 char is_multiple (DATA n, DATA *divisors, unsigned long num_divisors);
 
 /* BEGIN PROGRAM LOGIC */
 
-/* is_multiple(n,divisors,num_divisors)
- * Checks if a given number is a multiple of any one (or more) of the
- * arbitrary values given in an array.
+/* is_multiple(n, divisors, num_divisors)
+ * Checks if a given number is a multiple of any one of the
+ * arbitrary values given in the provided array.
  */
 char is_multiple (DATA n, DATA *divisors, unsigned long num_divisors)
 { 
-  unsigned long i; for (i = 0; i < num_divisors; i++) 
-                     if (fmod(n,divisors[i]) == 0) return 1; 
+  unsigned long i; 
+  for (i = 0; i < num_divisors; i++) 
+    if (fmod(n,divisors[i]) == 0) return 1; 
   return 0; 
 }
 
@@ -34,30 +36,30 @@ char is_multiple (DATA n, DATA *divisors, unsigned long num_divisors)
  */
 int main (int argc, char **argv)
 {
-  /* Variable declaration and argument check */
+  /* Variable declarations and setup */
   DATA max, sum, n, *divisors, matches;
-  unsigned long i, num_divisors, last_sum, overflows;
-  sum = 0; overflows = 0; matches = 0;
-  if (argc < 2) { printf("usage: %s [max] [divisor(s)]\n",argv[0]); return 0; }
+  unsigned long i, num_divisors;
+  sum = 0; matches = 0;
 
   /* Argument parsing */
+  if (argc < 2) { printf("usage: %s [max] [divisor(s)]\n",argv[0]); return 0; }
   max = atof(argv[1]);
   num_divisors = argc-2;
   divisors = (DATA *)calloc(num_divisors,sizeof(DATA));
   for (i = 0; i < num_divisors; i++) divisors[i] = atof(argv[i+2]);
 
-  /* Print initial statistics */
-  printf("Testing range:            0 < n < %.0Lf\n",max);
-  printf("Data size:                %lu bits\n",(unsigned long)sizeof(DATA)*8);
+  /* Pre-run statistics */
+  printf("Testing range:    0 < n < " DATA_FORMAT "\n",max);
+  printf("Data size:        %lu bits\n",(unsigned long)sizeof(DATA)*8);
 
-  /* Main computation loops */
-  for (n = 1; n < max; n++) if (is_multiple(n,divisors,(unsigned long)argc-2)) 
-  { last_sum = sum; matches++; sum += n; if (sum < last_sum) overflows++; }
+  /* Main computation loop */
+  for (n = 1; n < max; n++) 
+    if (is_multiple(n,divisors,(unsigned long)argc-2)) 
+    { matches++; sum += n; }
 	
   /* Result return and cleanup */ 
-  printf("Matching values in range: %.0Lf\n",matches);
-  printf("Sum of matching values:   %.0Lf\n",sum);
-  if (overflows) printf("WARNING: Overflow events (%lu) detected\n",overflows);
+  printf("Matches in range: " DATA_FORMAT "\n",matches);
+  printf("Sum of matches:   " DATA_FORMAT "\n",sum);
   free(divisors);
   return 0;
 }
