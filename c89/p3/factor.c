c80c58d6a607a2deb56a883bcbfa404d6cc9df06
ict <ict@nfinit.systems>
2019-10-29 11:25:31 -0600

    P3: Preliminary solution (C89)
    Basic untested solution for Problem 3 using trial division in C89

---
+++ c89/p3/factor.c
@@ -0,0 +1,65 @@
+/* PROBLEM 3 [ https://projecteuler.net/problem=3 ]
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600,851,475,143?
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+ 
+/* INCLUDES */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* TYPE DEFINITIONS */
+typedef long double   DATA;
+#define DATA_FORMAT   "%.0Lf"
+typedef unsigned long INDEX;
+#define INDEX_FORMAT  "%lu"
+
+/* OUTPUT STRINGS */
+#define USAGE_STR   "usage: %s [value]\n"
+#define ARGS_FAIL   "ERROR: %s only operates on positive integers!\n"
+#define MALLOC_FAIL "ERROR: Memory allocation failure during optimization!\n"
+
+/* FUNCTION PROTOTYPES */
+void trial_factor (DATA n)
+
+/* BEGIN PROGRAM * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* trial_factor(n)
+ * Factors n by trial division
+ */
+void trial_factor (DATA n)
+{
+  DATA f; f = 2;
+  while (n > 1)
+  {
+    if (fmod(n,f) == 0) { printf(DATA_FORMAT " ",f); n /= f; }
+    else f++;
+  }
+  printf("\n");
+}
+
+/* MAIN FUNCTION
+ * Takes in the maximum value as the first argument and arbitrary 
+ * target divisors in subsequent arguments. Keeps track of and
+ * prints sum of matching numbers when loop is completed.
+ */
+int main (int argc, char **argv)
+{
+  /* Variable declarations and setup */
+  DATA n;
+
+  /* Argument parsing */
+  if (argc < 1) { printf(USAGE_STR,argv[0]); return 0; }
+  n = floor(atof(argv[1]))-1;
+  if (n < 1) { printf(ARGS_FAIL,argv[0]); return 0; }
+
+  /* Print pre-run statistics */
+  printf("Factoring: " DATA_FORMAT "\n",n);
+  printf("Data size: " INDEX_FORMAT " bits\n",(INDEX)sizeof(DATA)*8);
+
+  /* Factor n */
+  trial_factor(n);
+
+  /* End of program */ 
+  return 0;
+}
